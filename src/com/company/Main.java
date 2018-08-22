c03a1011028bd7695481ad320fd8b4475e9c8bde
bensonresseler <bresseler.vdab@gmail.com>
2018-08-22 16:08:41 +0200

    Generic voorbeeld

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,32 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int [] getallen = new int[3];
-        int positie = 0;
+        DynArray<Integer> getallen = new DynArray<Integer>();
         int getal;
 
         do {
             System.out.print("Geef getal in (negatief om te stoppen): ");
             getal = Integer.parseInt(scanner.nextLine());
             if (getal >= 0) {
-                if (positie == getallen.length) {
-                    getallen = vergrootArray(getallen);
-                }
-                getallen[positie] = getal;
-                positie++;
+                getallen.add(getal);
             }
         } while(getal >= 0);
-
-        Arrays.sort(getallen,0,positie);
-        for (int i = 0; i < positie; i++) {
-            System.out.println(getallen[i]);
+        getallen.sort();
+        for (int i = 0; i < getallen.size(); i++) {
+            System.out.println(getallen.get(i));
         }
-    }
-
-    private static int[] vergrootArray(int[] getallen) {
-        int [] temp = new int[getallen.length+1];
-        System.arraycopy(getallen, 0, temp,0 , getallen.length);
-        getallen = temp;
-        return getallen;
     }
 }
